64982ecb4d6961aa6a11c0aa2b1852f07cbd2885
wangqiaoling <1113584801@qq.com>
2023-09-18 08:51:56 +0800

    fix: 修复段落组件

--- src/components/QuestionComponents/QuestionParagraph/Component.tsx
+++ src/components/QuestionComponents/QuestionParagraph/Component.tsx
@@ -4,11 +4,19 @@ import { Typography } from 'antd'
 
 const QuestionParagraph: FC<QuestionParagraphPropsType> = (props: QuestionParagraphPropsType) => {
     const { text = '', isCenter = false } = { ...QuestionParagraphDefaultProps, ...props }
+
+    const textList = text.split('\n')
+
     return (
         <Typography.Paragraph
             style={{ textAlign: isCenter ? 'center' : 'start', marginBottom: '10px' }}
         >
-            {text}
+            {textList.map((t, index) => (
+                <span key={index}>
+                    {index > 0 && <br />}
+                    {t}
+                </span>
+            ))}
         </Typography.Paragraph>
     )
 }
