755513f3c59fd87f0937ef0baf712a3f8543d7f8
wangqiaoling <1113584801@qq.com>
2023-10-12 08:13:10 +0800

    feat: 分包

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,38 @@
 module.exports = {
+    webpack: {
+        configure(webpackConfig) {
+            if (webpackConfig.mode === 'production') {
+                // 抽离公共代码，只在生产环境
+                if (webpackConfig.optimization == null) {
+                    webpackConfig.optimization = {}
+                }
+                webpackConfig.optimization.splitChunks = {
+                    // 对所有的包都使用
+                    chunks: 'all',
+                    cacheGroups: {
+                        antd: {
+                            name: 'antd-chunk',
+                            test: /antd/,
+                            // 权限越高，优先级越高，越优先匹配
+                            priority: 100,
+                        },
+                        reactDom: {
+                            name: 'reactDom-chunk',
+                            test: /react-dom/,
+                            priority: 99,
+                        },
+                        // 所有的第三方库都引自 node_modules
+                        vendors: {
+                            name: 'vendors-chunk',
+                            test: /node_modules/,
+                            priority: 98,
+                        },
+                    },
+                }
+            }
+            return webpackConfig
+        },
+    },
     devServer: {
         port: 8000,
         proxy: {
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
 import ManageLayout from '../layouts/ManageLayout'
@@ -9,9 +9,11 @@ import NotFound from '../pages/NotFound'
 import List from '../pages/manage/List'
 import Star from '../pages/manage/Star'
 import Trash from '../pages/manage/Trash'
-import Edit from '../pages/question/Edit'
-import Stat from '../pages/question/Stat'
 import Register from '../pages/Register'
+
+// 路由懒加载，拆分 bundle，优化首页体积
+const Edit = lazy(() => import(/* webpackChunkName: "editPage"*/ '../pages/question/Edit'))
+const Stat = lazy(() => import(/* webpackChunkName: "statPage"*/ '../pages/question/Stat'))
 
 const router = createBrowserRouter([
     {
